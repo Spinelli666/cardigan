c77f3458f2587cbbaaa923527ae229dc771f7405
Bernardo Gonçalves Spinelli Martins <bernardospinelli@hotmail.com.br>
2025-07-06 23:42:23 -0300

    (feat): reorganize Features tab layout and add character details

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -10,15 +10,25 @@ export default class CardiganSystemActorBase extends foundry.abstract
     schema.health = new fields.SchemaField({
       value: new fields.NumberField({
         ...requiredInteger,
-        initial: 10,
+        initial: 0,
         min: 0,
       }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 0 }),
     });
     schema.power = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 5 }),
+      value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 0 }),
     });
+    
+    // Character details
+    schema.details = new fields.SchemaField({
+      name: new fields.StringField({ initial: "" }),
+      age: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      race: new fields.StringField({ initial: "" }),
+      movement: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+      criticalHit: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+    });
+    
     schema.biography = new fields.HTMLField();
 
     return schema;
