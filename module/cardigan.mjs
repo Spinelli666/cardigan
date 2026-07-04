7a5b033da4007f45c3b2d20e690cb70b71becc2d
Bernardo Gonçalves Spinelli Martins <bernardospinelli@hotmail.com.br>
2026-07-03 23:56:52 -0300

    refact: extrair armor-info-badges.hbs como partial reutilizável (equipment.hbs)

--- module/cardigan.mjs
+++ module/cardigan.mjs
@@ -150,6 +150,7 @@ Hooks.once('init', function () {
     'systems/cardigan/templates/actor/partials/skill-row.hbs',
     'systems/cardigan/templates/actor/partials/recipe-row.hbs',
     'systems/cardigan/templates/actor/partials/durability-display.hbs',
+    'systems/cardigan/templates/actor/partials/armor-info-badges.hbs',
   ]);
 });
 
